ef732e7a45de61bdc643a514332518f945469d1d
aj <a.seleznev@oiler.com.ua>
2021-02-19 18:38:22 +0200

    1.1 task
    Signed-off-by: aj <a.seleznev@oiler.com.ua>

---
+++ app/code/Training/Dependency2/registration.php
@@ -0,0 +1,10 @@
+<?php
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Training_Dependency2', __DIR__);
+
+/*$registrar = new ComponentRegistrar();
+if ($registrar->getPath(ComponentRegistrar::MODULE, 'Magento_TestModuleDefaultHydrator') === null) {
+    ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_TestModuleDefaultHydrator', __DIR__);
+}*/
+
+
